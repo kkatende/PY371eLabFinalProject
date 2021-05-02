e22a968215536b50d4c393cd14115a9a3244275b
kkatende <83525833+kkatende@users.noreply.github.com>
2021-05-01 23:34:27 -0400

    Add files via upload

---
+++ py371proj1.py
@@ -0,0 +1,78 @@
+#
+#
+# PY371: Electronic Lab for Students
+#
+# Final Project: Moon Diagram
+# Part I: Raspberry Pi - Python 3
+#
+
+from hmcpng import *
+import serial
+
+# arduino = serial.Serial(port='COM4', baudrate=115200, timeout=.1)
+# FIX THE ABOVE!!!!! PRONTO
+
+
+def create_uniform_image(height, width, pixel):
+    """ creates and returns a 2-D list of pixels with height rows and
+        width columns in which all of the pixels have the RGB values
+        given by pixel
+        inputs: height and width are non-negative integers
+                pixel is a 1-D list of RBG values of the form [R,G,B],
+                     where each element is an integer between 0 and 255.
+    """
+    pixels = []
+
+    for r in range(height):
+        row = [pixel] * width
+        pixels += [row]
+
+    return pixels
+
+def blank_image(height, width):
+    """ creates and returns a 2-D list of pixels with height rows and
+        width columns in which all of the pixels are green.
+        inputs: height and width are non-negative integers
+    """
+    all_green = create_uniform_image(height, width, [0, 255, 0])
+    return all_green
+
+def brightness(pixel):
+    """ takes a pixel (an [R, G, B] list) and returns a value
+        between 0 and 255 that represents the brightness of that pixel.
+    """
+    red = pixel[0]
+    green = pixel[1]
+    blue = pixel[2]
+    return (21*red + 72*green + 7*blue) // 100
+
+def bw(pixels, threshold):
+    """ Takes the 2-D list pixels containing pixels for an image, and 
+        that creates and returns a new 2-D list of pixels for an image 
+        that is a black-and-white version of the original image.
+        inputs: an integer threshold between 0 and 255 that should 
+                govern which pixels are turned white and which are 
+                turned black.
+    """
+    bwimg = blank_image(len(pixels),len(pixels[0]))
+    
+    for r in range(len(pixels)):
+        for c in range(len(pixels[0])):
+            if brightness(pixels[r][c]) > threshold:
+                bwimg[r][c] = [255,255,255]
+            else:
+                bwimg[r][c] = [0,0,0]
+                
+    return bwimg
+
+def LEDdisplay(pixels):
+    """ Creates a new image from the original, changing color values from 
+        color to black and white based on brightness, then converts it into
+        a byte array for processing by an Arduino UNO.
+    """
+                    
+    ser=serial.Serial("/dev/ttyACM0",9600)
+    ser.baudrate=9600
+    pixels = bw(pixels, min(brightness(pixel)))
+    img = pixels.tobitmap()
+    return img
